72d890bca50dee1bd163fa784f93b2d9db714c10
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2025-05-26 17:05:13 +0900

    feat: cors

--- src/main/java/com/alfarizi/budgetin/config/SecurityConfig.java
+++ src/main/java/com/alfarizi/budgetin/config/SecurityConfig.java
@@ -42,12 +42,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://budgetin-one.vercel.app")); // ganti sesuai origin frontend kamu
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true); // kalau pakai JWT di cookie atau Authorization header
-        config.setMaxAge(3600L); // tambahkan ini untuk cache preflight
-        
+
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
