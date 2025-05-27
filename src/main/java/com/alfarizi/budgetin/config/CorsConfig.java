5899eccdff159b94d3539b9c7a21e0ff4e21cbf0
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2025-05-27 10:31:19 +0900

    change env

--- src/main/java/com/alfarizi/budgetin/config/CorsConfig.java
+++ src/main/java/com/alfarizi/budgetin/config/CorsConfig.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed.origin:https://budgetin-one.vercel.app}")
+    @Value("${app.cors.allowed.origin}")
     private String allowedOrigin;
 
     @Bean
